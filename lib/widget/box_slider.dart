3e82aa6574ad92dcbf16be0720a1e18b30b86462
hongseop <a01082372487@gmail.com>
2023-11-21 15:56:42 +0900

    ADD :: makeBoxImages 함수 생성

---
+++ lib/widget/box_slider.dart
@@ -0,0 +1,45 @@
+import 'package:clone_netflix/model/model_movie.dart';
+import 'package:flutter/material.dart';
+
+class BoxSlider extends StatelessWidget {
+  final List<Movie> movies;
+  const BoxSlider({super.key, required this.movies});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(7),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Text('지금 또는 콘텐츠'),
+          Container(
+            height: 120,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: makeBoxImages(movies),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+List<Widget> makeBoxImages(List<Movie> movies) {
+  List<Widget> results = [];
+  for (var i = 0; i < movies.length; i++) {
+    results.add(
+      InkWell(
+        onTap: () {},
+        child: Container(
+          padding: const EdgeInsets.only(right: 10),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Image.asset('images/${movies[i].poster}'),
+          ),
+        ),
+      ),
+    );
+  }
+  return results;
+}
