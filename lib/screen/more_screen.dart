1b04149cb77da3ca689853f63799d52c03dd2f21
hongseop <a01082372487@gmail.com>
2023-12-23 15:32:49 +0900

    ADD :: 오랜만

--- lib/screen/more_screen.dart
+++ lib/screen/more_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MoreScreen extends StatelessWidget {
   @override
@@ -24,7 +26,54 @@ class MoreScreen extends StatelessWidget {
                       fontSize: 25,
                       color: Colors.white),
                 ),
-              )
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                width: 140,
+                height: 5,
+                color: Colors.red,
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: Linkify(
+                  onOpen: (link) async {
+                    if (await canLaunchUrl(link.url as Uri)) {
+                      await launchUrl(link.url as Uri);
+                    }
+                  },
+                  text: 'https://github.com/junghongseop',
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 20),
+                  linkStyle: const TextStyle(color: Colors.white),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Container(
+                    color: Colors.red,
+                    width: 180,
+                    height: 35,
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.edit,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          '프로필 수정하기',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
